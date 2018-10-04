66d1553f5cd17a0d014c7edc097b4f5d9e8e7e01
Lucas Antonio Ramos Sartori <faltou.criatividade0@gmail.com>
2018-10-04 19:08:51 -0300

    abstract entity and operations to CRUD createds

--- src/app/core/data-service/abstract.service.ts
+++ src/app/core/data-service/abstract.service.ts
@@ -1,7 +1,8 @@
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import {AbstractEntity} from '../../model/abstract-entity';
 
-export abstract class AbstractService<T> {
+export abstract class AbstractService<T extends AbstractEntity> {
 
   protected constructor(private http: HttpClient, private url: string) {
   }
@@ -12,5 +13,13 @@ export abstract class AbstractService<T> {
 
   create(model: T): Observable<T> {
     return this.http.post<T>(this.url, JSON.stringify(model));
+  }
+
+  edit(model: T): Observable<T> {
+    return this.http.put<T>(this.url + model.id, JSON.stringify(model));
+  }
+
+  destroy(model: T): Observable<T> {
+    return this.http.delete<T>(this.url + model.id);
   }
 }
---
+++ src/app/model/abstract-entity.ts
@@ -0,0 +1,5 @@
+export abstract class AbstractEntity {
+
+  protected constructor(public id?: number, public createdAt?: Date, public updatedAt?: Date) {
+  }
+}
